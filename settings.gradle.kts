60bb0ccb5e5122c91ac5ccaa94556c4097b0557e
kingjjy-debug <kingjjy@gmail.com>
2025-10-14 13:15:24 +0900

    fix(ci): stable license accept and declare plugin versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,14 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.application") version "8.6.0"
+        id("org.jetbrains.kotlin.android") version "1.9.24"
+    }
+}
+
 rootProject.name = "HelloAndroid"
 include(":app")
