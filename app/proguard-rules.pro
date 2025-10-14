8f09db454cada117395d2b0fefca35d9fd004b17
kingjjy-debug <kingjjy@gmail.com>
2025-10-14 11:55:39 +0900

    Initial Hello Android project

---
+++ app/proguard-rules.pro
@@ -0,0 +1,1 @@
+# Proguard rules (현재 비어 있음)
---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    // 루트에는 특별한 플러그인 없음. (모듈에 적용)
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
